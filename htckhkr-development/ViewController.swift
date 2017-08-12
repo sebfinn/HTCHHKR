3943e44ecf841773656cfd6f230faba8e4dd4b00
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-08-12 11:42:25 -0700

    Initial Commit

---
+++ htckhkr-development/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  htckhkr-development
+//
+//  Created by Sebastian DiPirro on 8/12/17.
+//  Copyright © 2017 Sebastian DiPirro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
